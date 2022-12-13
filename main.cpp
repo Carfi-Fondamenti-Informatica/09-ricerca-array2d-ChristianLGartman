a7969a06ffa467ba8cd8ba4f060fe91009553a9e
ChristianLGartman <115482491+ChristianLGartman@users.noreply.github.com>
2022-12-13 22:58:34 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -5,10 +5,8 @@ int main() {
     char nomi[10][20];
     char ricerca[20];
     for(int i =0; i<10; i++){
-        cout<<"Dimmi un nome:"<<endl;
         cin >> nomi[i];
         }
-    cout<<"Dimmi il nome che cerchi:"<<endl;
     cin >> ricerca;
     int s = funzione (nomi, ricerca);
     if (s<=10){
